07541038f6885699ab1f41b7720b4efa6e7026e9
Hestrela <heitorestrela13@gmail.com>
2026-04-05 12:27:03 -0300

    feat: add getter and setters

--- CP3-Heitor/Playlist.java
+++ CP3-Heitor/Playlist.java
@@ -2,7 +2,42 @@ import java.util.ArrayList;
 
 public class Playlist {
 
-    String nome;
-    ArrayList<Musica> musicas;
+    private String nome;
+    private ArrayList<Musica> musicas;
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) {
+        if (nome == null) {
+            System.out.println("O nome da playlist não deve ser nulo!");
+        } else {
+            String nomeCorreto = nome.trim();
+            if (nomeCorreto.isEmpty()) {
+                System.out.println("O nome não deve ser vazio!");
+            } else {
+                this.nome = nomeCorreto;
+            }
+        }
+    }
+
+    public ArrayList<Musica> getMusicas() {
+        return musicas;
+    }
+
+    public void adicionarMusicas(Musica musica) {
+        if (musica == null) {
+            System.out.println("A música não deve ser nula!");
+        } else {
+            musicas.add(musica);
+        }
+    }
+
+    public void removerMusica(int indice) {
+        if (indice >= musicas.size() || indice < 0) {
+            System.out.println("O índice não deve ser negativo ou maior que o tamanho da lista de músicas");
+        } else {       
+                musicas.remove(indice);         
+        }
+    }
 
 }
