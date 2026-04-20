5d22793628e93d9dc2fa63c91e42a92ba6ea4902
Hestrela <heitorestrela13@gmail.com>
2026-04-20 18:53:50 -0300

    fix: error in examples

--- CP4-Heitor/Usuario.java
+++ CP4-Heitor/Usuario.java
@@ -7,7 +7,33 @@ public class Usuario {
     protected ArrayList<Playlist> playlists;
     protected ArrayList<Musica> historicoReproducao;
     
-    public String getNome() {
+
+    public Usuario(String nome, String email) {
+        setNome(nome);
+        setEmail(email);
+        this.playlists = new ArrayList<>();
+        this.historicoReproducao = new ArrayList<>();
+    }
+    
+    public void reproduzirMusica(Musica musica) {
+        System.out.println("🎵 Reproduzindo: " + musica.getTitulo());
+        historicoReproducao.add(musica);
+    }
+    
+    public void exibirHistorico() {
+        System.out.println("\n--- HISTÓRICO DE REPRODUÇÃO ---");
+        for (Musica m : historicoReproducao) {
+            m.exibir();
+        }
+    }
+    
+    public void criarPlaylist(String nome) {
+        Playlist playlist = new Playlist(nome);
+        playlists.add(playlist);
+        System.out.println("✅ Playlist criada!");
+    }
+
+     public String getNome() {
         return nome;
     }
 
@@ -38,25 +64,4 @@ public class Usuario {
     public void setHistoricoReproducao(ArrayList<Musica> historicoReproducao) {
         this.historicoReproducao = historicoReproducao;
     }
-
-    public Usuario(String nome, String email) {
-        setNome(nome);
-        setEmail(email);
-        this.playlists = new ArrayList<>();
-        this.historicoReproducao = new ArrayList<>();
-    }
-    
-    public void reproduzirMusica(Musica musica) {
-        System.out.println("🎵 Reproduzindo: " + musica.getTitulo());
-        historicoReproducao.add(musica);
-    }
-    
-    public void exibirHistorico() {
-        System.out.println("\n--- HISTÓRICO DE REPRODUÇÃO ---");
-        for (Musica m : historicoReproducao) {
-            m.exibir();
-        }
-    }
-    
-    // Getters e Setters...
 }
