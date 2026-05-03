97d63d342b5500c7bdf9499fe8d306c09b187a88
Hestrela <heitorestrela13@gmail.com>
2026-05-02 21:20:31 -0300

    feat: Estrutura base da playlist automática

---
+++ CP5-Heitor/PlaylistAutomatica.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class PlaylistAutomatica extends Playlist{
+    private String criterio;
+
+    @Override
+    public void reproduzir() {
+        System.out.println("🤖 Playlist Automática: " + nome);
+        System.out.println("📊 Critério: " + criterio);
+        super.reproduzir();
+    }
+
+    public void atualizar(ArrayList<Musica> todasMusicas) {
+        musicas.clear();
+        
+        if (criterio.equals("top")) {
+            // Adicionar músicas mais tocadas
+        } else if (criterio.equals("recomendadas")) {
+            // Adicionar músicas recomendadas
+        }
+    }
+}
